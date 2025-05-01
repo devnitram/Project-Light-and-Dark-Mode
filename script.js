9c9c917a455a6110c3ca45232e48b22b8561c9ea
nitram <weslleybarros@gmail.com>
2025-05-01 00:56:05 -0300

    Atualização no botão de ativar e desativar o dark mode - Adicionado animação.

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function changeTheme() {
   //depois de pegar substituir a imagem
   if(html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
-    image.setAttribute('src', './assets/macacolight.png')
+    image.setAttribute('src', './assets/bg-white.avif')
   } else {
     // se tiver sem o light mode, manter a imagem normal
-    image.setAttribute('src', './assets/avatardark.png')
+    image.setAttribute('src', './assets/bg-black.jpg')
   }
 
 
